e139e871087625f9b32b4bae49bae8e47f809e56
Jiaqi Duan <jduan10@ucsc.edu>
2024-03-03 15:36:58 -0800

    edited intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -117,7 +117,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://music.163.com/#/user/home?id=641857303"
+          href="https://music.163.com/#/artist?id=46274436"
           target="_blank"
         >
           <RiNeteaseCloudMusicFill />
